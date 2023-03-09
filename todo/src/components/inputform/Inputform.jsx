f0e32c67f8d4247f234851dbc5ff0084cef2ebd5
phandyph <phornphandy23@gmail.com>
2023-03-09 17:26:52 +0700

    #2 input-form

--- todo/src/components/inputform/Inputform.jsx
+++ todo/src/components/inputform/Inputform.jsx
@@ -1,8 +1,28 @@
 import './Inputform.css';
 
-const Inputform = () => {
+const Inputform = ({
+    onAdd,
+    todos,
+    task,
+    setTask
+}) => {
     return (
-        <div>Form</div>
+        // onSubmit={e => e.preventDefault()} : just enter we can submit data.
+        <form onSubmit={e => e.preventDefault()} className='round'>
+            <input 
+                value={task}
+                onChange={(e)=>setTask(e.target.value)}
+                type="text" 
+                placeholder="Add todo"
+            />
+
+            {/* can submit using button. One more is by enter key */}
+            <button 
+                type='submit'
+                className='right bg-success text-white'
+                onClick={() => onAdd()}
+            >+</button>
+        </form>
     )
 }
 
